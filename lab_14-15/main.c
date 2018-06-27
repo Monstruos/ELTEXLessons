b187be6896345421b7286ce8eb17416f726f4518
Ivan Sanin <mongstruos@mail.ru>
2018-06-27 11:23:53 +0700

    Занятие 15, правка .gitignore
    Совместил 14 и 15 занятия в одну папку.

--- lab_14-15/main.c
+++ lab_14-15/main.c
@@ -7,7 +7,7 @@
 void clear_stdin_buffer()
 {
     char input;
-    while((input = fgetc(stdin)) != EOF && input != '\n');
+    while ((input = fgetc(stdin)) != EOF && input != '\n');
 }
 
 
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
     float result;
     int menu_selector = -1;
 
-    while(1) {
+    while (1) {
         printf("1) add(a, b);\n");
         printf("2) sub(a, b);\n");
         printf("3) multi(a, b);\n");
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[])
         printf("b?");
         scanf("%i", &second_number);
         clear_stdin_buffer();
-        switch(menu_selector) {
+        switch (menu_selector) {
             case 1: {
                 result = add(first_number, second_number);
                 break;
