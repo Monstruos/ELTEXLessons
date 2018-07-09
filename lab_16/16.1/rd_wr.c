4c36df43e0889ce5282e230bf39fdf13ede7a55d
Ivan Sanin <mongstruos@mail.ru>
2018-07-09 13:20:21 +0700

    Правка занятия 16 (часть 1)

--- lab_16/16.1/rd_wr.c
+++ lab_16/16.1/rd_wr.c
@@ -8,14 +8,16 @@
 
 int main(int argc, char const *argv[]) {
     int rdwr_f_descr = open(argv[1], O_RDWR);
+    struct stat f_stat;
+    stat(argv[1], &f_stat);
+    int size = f_stat.st_size;
     char out_buffer[BUF_SIZE] = "";
-    read(rdwr_f_descr, out_buffer, BUF_SIZE);
-    int str_size = strlen(out_buffer);
-    for (int i = 0; i < (str_size - 1) / 2; i++) {
-        out_buffer[i] ^= out_buffer[str_size - 1 - i] ^=
-        out_buffer[i] ^= out_buffer[str_size - 1 - i];
+    int offset;
+    for (offset = 1; offset <= size; ++offset) {
+        lseek(rdwr_f_descr, -offset, SEEK_END);
+        read(rdwr_f_descr,  &out_buffer[offset - 1], 1);
     }
     write(STDOUT_FILENO, out_buffer, strlen(out_buffer));
-    close(rdwr_f_descr)
+    close(rdwr_f_descr);
     return 0;
 }
