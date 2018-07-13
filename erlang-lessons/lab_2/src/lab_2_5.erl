2a0eff6bbae264a2d18d232c576789245c339a0a
Ivan Sanin <mongstruos@mail.ru>
2018-07-13 22:57:58 +0700

    Hotfix Задание 2 (erlang)

--- erlang-lessons/lab_2/src/lab_2_5.erl
+++ erlang-lessons/lab_2/src/lab_2_5.erl
@@ -6,9 +6,9 @@
 % example: lab_2_5:ackermann(3, 3). if you wanna have a cup of coffee, you can
 % run ackermann(4, 2).
 
-ackermann(0, N) ->
+ackermann(0, N) when N > 0->
     N + 1;
-ackermann(M, 0) ->
+ackermann(M, 0) when M > 0 ->
     ackermann(M - 1, 1);
-ackermann(M, N) ->
+ackermann(M, N) when M > 0, N > 0 ->
     ackermann(M - 1, ackermann(M, N - 1)).
