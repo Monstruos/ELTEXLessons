15682f73dffa6b71105e78890a08308f46c0bb49
Ivan Sanin <mongstruos@mail.ru>
2018-07-16 13:03:23 +0700

    hotfix (erlang lab_2)

--- erlang-lessons/lab_2/src/lab_2_4.erl
+++ erlang-lessons/lab_2/src/lab_2_4.erl
@@ -5,12 +5,7 @@
 % Reverse the list
 % example: lab_2_4:reverse([2, 3, 4, 6, 8, 2, 3, 21, 4]).
 
-reverse(List) ->
-    reverse(List, []).
-
-reverse([], []) ->
+reverse([]) ->
     [];
-reverse([], ResList) ->
-    ResList;
-reverse([H|T], ResList) ->
-    reverse(T, [H|ResList]).
+reverse([H|T]) ->
+    reverse(T) ++ [H].
--- erlang-lessons/lab_2/src/lab_2_5.erl
+++ erlang-lessons/lab_2/src/lab_2_5.erl
@@ -6,7 +6,7 @@
 % example: lab_2_5:ackermann(3, 3). if you wanna have a cup of coffee, you can
 % run ackermann(4, 2).
 
-ackermann(0, N) when N > 0->
+ackermann(0, N) when N >= 0->
     N + 1;
 ackermann(M, 0) when M > 0 ->
     ackermann(M - 1, 1);
